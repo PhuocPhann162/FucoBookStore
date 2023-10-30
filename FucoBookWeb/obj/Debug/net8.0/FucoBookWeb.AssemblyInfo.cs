793b6d6987a720176fe3f5dbfcd7e11f7769e8a0
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-10-30 16:47:35 +0700

    update connectionString for deploy on Azure

--- FucoBookWeb/obj/Debug/net8.0/FucoBookWeb.AssemblyInfo.cs
+++ FucoBookWeb/obj/Debug/net8.0/FucoBookWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FucoBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f600d6e1a14085585bd27d50c0bbc3c4082f59fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FucoBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FucoBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
